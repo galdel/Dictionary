2f2aa3c6dec579872cf23a671f713d3bd93690ab
galdel <galdel@ya.ru>
2022-04-09 03:25:08 +0300

    2

--- src/com/ag/Main.java
+++ src/com/ag/Main.java
@@ -1,8 +1,25 @@
 package com.ag;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	    System.out.println("Hi");
+        HashMap<String, String> words = new HashMap<>();
+        words.put("Almost","Почти");
+        words.put("Afraid","Бояться");
+        System.out.println(words);
+        FileWriter writer = new FileWriter("words.txt");
+        writer.write("Доктор");
+        for (Map.Entry<String, String> entry : words.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            writer.write(key + " = " + value + System.lineSeparator());
+        }
+        writer.close();
     }
 }
