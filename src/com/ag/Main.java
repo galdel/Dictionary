1d0eb1e7d0dd7837c0fe63be386738c8314085b4
galdel <galdel@ya.ru>
2022-04-16 02:33:05 +0300

    4

--- src/com/ag/Main.java
+++ src/com/ag/Main.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -14,18 +12,40 @@ public class Main {
         String fileName="words.txt";
         HashMap<String, String> words = new ReadWords(fileName).read();
         new WriteWords(words, fileName).write();
-        for (Map.Entry<String, String> entry : words.entrySet()) {
-            System.out.println(entry.getKey());
-            Scanner in = new Scanner(System.in);
-            System.out.print("Ответ: ");
-            String name = in.nextLine();
-            if (name.equals(entry.getValue())) {
-                System.out.println("Верно!");}
-            else {
-                System.out.println("Не верно!");
-                System.out.println(entry.getValue());
+        ArrayList<String> keyWords = new ArrayList<>(words.keySet());
+        int index = 0;
+        while (true) {
+            Random generator = new Random();
+            if (generator.nextBoolean() == true){
+                int rnd = generator.nextInt(words.keySet().toArray().length);
+                String key = words.keySet().toArray()[rnd].toString();
+                String value = words.values().toArray()[rnd].toString();
+                System.out.println(key);
+                Scanner in = new Scanner(System.in);
+                System.out.print("Ответ: ");
+                String name = in.nextLine();
+                if (name.toLowerCase().equals(value.toLowerCase())) {
+                    System.out.println("Верно!");}
+                else {
+                    System.out.print("Не верно! Верно так: ");
+                    System.out.println(value.toUpperCase());
+                }
             }
-
+            else {
+                int rnd = generator.nextInt(words.keySet().toArray().length);
+                String key = words.keySet().toArray()[rnd].toString();
+                String value = words.values().toArray()[rnd].toString();
+                System.out.println(value);
+                Scanner in = new Scanner(System.in);
+                System.out.print("Ответ: ");
+                String name = in.nextLine();
+                if (name.toLowerCase().equals(key.toLowerCase())) {
+                    System.out.println("Верно!");}
+                else {
+                    System.out.print("Не верно! Верно так: ");
+                    System.out.println(key.toUpperCase());
+                }
+            }
         }
     }
 
